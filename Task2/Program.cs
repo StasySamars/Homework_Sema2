a9a553c5a464c4e8da782868cccc03d2c88e3065
StasySamars <119619342+StasySamars@users.noreply.github.com>
2022-12-19 21:17:31 +0300

    erwr

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,6 +1,16 @@
-﻿/*Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+﻿/*Напишите программу, которая выводит случайное трехзначное число 
+и удаляет вторую цифру этого числа.
 
 456 -> 46
 782 -> 72
 918 -> 98*/
 
+Console.Clear();
+int number = new Random().Next(100, 1000);
+
+Console.WriteLine (number);
+
+int num1 = number/100;
+int num2 = number%10;
+Console.WriteLine (num1);
+Console.WriteLine (num2);
