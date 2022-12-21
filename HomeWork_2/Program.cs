849d1348ce59fa11c1a468d095e2cb7978aafa2a
StasySamars <119619342+StasySamars@users.noreply.github.com>
2022-12-21 19:54:53 +0300

    homework done

---
+++ HomeWork_2/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/
+
+/*Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+} 
+*/
+
+Console.Clear();
+Console.WriteLine("Введите число и я выведу третью цифру заданного числа или сообщу, что третьей цифры нет: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 100)
+    {
+    Console.WriteLine("Третьей цифры нет! :)");
+    }
+else
+{
+    while (num > 1000)
+    {
+        num = num/10;
+    }
+    if (num > 99 && num < 1000)
+    {
+        int number = (num % 10);
+        Console.WriteLine($"{number} - Готово!");
+    }
+}
